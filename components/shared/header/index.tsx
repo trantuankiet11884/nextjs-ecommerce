4e1f639aba95bf6e5fa1979036a98faa68a38006
trantuankiet11884 <trantuankiet11884@gmail.com>
2024-12-30 22:15:11 +0700

    feat: drawer categories side bar

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,21 +1,23 @@
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
-import Search from "./search";
 import Menu from "./menu";
 import data from "@/lib/data";
-import { Button } from "@/components/ui/button";
-import { MenuIcon } from "lucide-react";
+import Search from "./search";
+import Sidebar from "./sidebar";
+import { getAllCategories } from "@/lib/actions/products.actions";
 
-export default function Header() {
+export default async function Header() {
+  const categories = await getAllCategories();
+
   return (
-    <header className="bg-black text-white">
+    <header className="bg-black  text-white">
       <div className="px-2">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <Link
               href="/"
-              className="flex items-center header-button font-extrabold text-2xl m-1"
+              className="flex items-center header-button font-extrabold text-2xl m-1 "
             >
               <Image
                 src="/icons/logo.svg"
@@ -35,19 +37,13 @@ export default function Header() {
           <Search />
         </div>
       </div>
-      <div className="flex items-center px-3 mb-[1px] bg-gray-800">
-        <Button
-          variant="ghost"
-          className="dark header-button flex items-center gap-1 text-base [&_svg]:size-6"
-        >
-          <MenuIcon />
-          Tất cả
-        </Button>
-        <div className="flex items-center flex-wrap gap-3 overflow-hidden max-h-[42px]">
+      <div className="flex items-center px-3 mb-[1px]  bg-gray-800">
+        <Sidebar categories={categories} />
+        <div className="flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]">
           {data.headerMenus.map((menu) => (
             <Link
-              key={menu.href}
               href={menu.href}
+              key={menu.href}
               className="header-button !p-2"
             >
               {menu.name}
